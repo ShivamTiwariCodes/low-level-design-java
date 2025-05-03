509a841bb277900748b5f690bf2f138fa073e04c
ShivamTiwariCodes <shivam67847@gmail.com>
2025-05-03 23:08:09 +0530

    adding structure

---
+++ src/main/java/common/utils/TextUtil.java
@@ -0,0 +1,18 @@
+package common.utils;
+
+import java.util.Random;
+
+public class TextUtil {
+
+    private static String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static Random random = new Random();
+
+    public static String generateRandomText(int length) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append(chars.charAt(random.nextInt(chars.length())));
+        }
+        return sb.toString();
+    }
+    
+}
